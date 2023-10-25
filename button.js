6b0e324a2088ed3137bb2976df446e108a0d4cf8
phoneemail <148524013+phoneemail@users.noreply.github.com>
2023-10-25 07:44:35 -0300

    draft

---
+++ button.js
@@ -0,0 +1,20 @@
+<div ng-app="myApp" ng-controller="myCtrl">
+  <button
+    style="display: flex; align-items: center; justify-content: space-between; padding: 10px 15px; background-color: #02BD7E; font-weight: bold; color: #ffffff; border: none; border-radius: 3px; font-size: inherit; cursor: pointer;"
+    id="btn_ph_login"
+    name="btn_ph_login"
+    type="button"
+    ng-click="openLoginWindow()"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style="margin-right: 5px; fill: #ffffff"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+    >
+      <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12Z" />
+    </svg>
+    Sign in with Phone Number
+  </button>
+</div>
