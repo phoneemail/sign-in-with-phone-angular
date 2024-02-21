7700049d2dbaafe2f0d1e5586c1cdd89ea9c621f
Devendra <dev@phone.email>
2024-02-21 17:35:40 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     countryCode: "+91",
     phoneNo: 'YOUR_PHONE_NUMBER',
-    redirectURL: 'https://example.com/auth',
+    redirectURL: 'YOUR_REDIRECT_URL',
     getEmailCountURl: 'https://eapi.phone.email/email-count',
     apiKey: 'YOUR_API_KEY'
 };
